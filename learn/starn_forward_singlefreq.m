ce3e1c09c54f15ca69d9935487ddb9e7d9a85bb8
MoZhou1995 <31834863+MoZhou1995@users.noreply.github.com>
2022-07-23 11:27:18 -0400

    save many data in one mat

--- learn/starn_forward_singlefreq.m
+++ learn/starn_forward_singlefreq.m
@@ -1,17 +1,17 @@
 % This script generates the data for a star shaped domain, for a fixed
 % number of sensors and incident directions where data is available for all
 % sensors at each incident direction
-function starn_forward_singlefreq(array_id)
+function starn_forward_singlefreq(mat_id)
 close all
-clearvars -except array_id
+clearvars -except mat_id
 tic
 if nargin == 0
-    fprintf('Runing the code on one server, no parallel computing \n')
-elseif array_id == 0
+    fprintf('Runing the code on one server \n')
+elseif mat_id == 0
     fprintf('Generating validation data \n')
 else
-    fprintf(['Generating training data through parallel computing with array index ' ...
-    num2str(array_id) ', will not generate validation data. \n'])
+    fprintf(['Generating training data through parallel computing with mat index ' ...
+    num2str(mat_id) ', will not generate validation data. \n'])
 end
 
 cfg_path = './configs/nc3.json';
@@ -79,7 +79,7 @@ if ndata>1 && ~exist(train_data_dir, 'dir')
     mkdir(train_data_dir)
 end
 
-if nargin == 0 || array_id == 0
+if nargin == 0 || mat_id == 0
     uscat_val = zeros(nvalid, n_dir, n_tgt);
     parfor idx=1:nvalid
         coefs = coefs_val(idx, :)';
@@ -105,37 +105,46 @@ if nargin == 0 || array_id == 0
     end
 end
 save_fcn = @(name, coefs, uscat) save(name, 'coefs', 'uscat');
+ndata_per_mat = cfg.ndata_per_mat;
 if nargin == 0
     fprintf('Start to generate training data \n')
-    parfor idx=1:ndata
-        data_name = strcat(train_data_dir, '/train_data_', num2str(idx),'.mat');
-        if ~exist(data_name, 'file')
-            coefs = sample_fc(cfg, 1)';
-            src_info = geometries.starn(coefs,nc,n);
+    nmat = ndata / ndata_per_mat;
+    parfor mat_index=1:nmat
+        data_start_index = (mat_index-1) * ndata_per_mat + 1;
+        data_end_index = mat_index * ndata_per_mat;
+        data_name = [train_data_dir '/train_data_' num2str(data_start_index) '-' num2str(data_end_index) '.mat'];
+        coefs = sample_fc(cfg, ndata_per_mat);
+        uscat = complex(zeros(ndata_per_mat, n_dir, n_tgt));
+        for local_idx = 1:ndata_per_mat
+            coef = coefs(local_idx, :)';
+            src_info = geometries.starn(coef,nc,n);
             [mats,~] = rla.get_fw_mats(kh,src_info,bc,sensor_info,opts);
             fields = rla.compute_fields(kh,src_info,mats,sensor_info,bc,opts);
-            uscat = reshape(fields.uscat_tgt, [n_dir, n_tgt]);
-            save_fcn(data_name, coefs, uscat);
+            uscat_temp = reshape(fields.uscat_tgt, [n_dir, n_tgt]);
+            uscat(local_idx,:,:) = uscat_temp;
         end
+        save_fcn(data_name, coefs, uscat);
     end
-elseif array_id >= 1
-    ndata_per_array = cfg.ndata_per_array;
-    start_idx = (array_id - 1) * ndata_per_array + 1;
-    end_idx = array_id * ndata_per_array;
-    end_idx = min(end_idx, ndata);
-    fprintf(['Start to generate training data indexed from ' num2str(start_idx) ...
-    ' to ' num2str(end_idx) '\n'])
-    for idx=start_idx:end_idx
-        data_name = strcat(train_data_dir, '/train_data_', num2str(idx),'.mat');
-        if ~exist(data_name, 'file')
-            rng(idx)
-            coefs = sample_fc(cfg, 1)';
-            src_info = geometries.starn(coefs,nc,n);
+elseif mat_id >= 1
+    data_start_index = (mat_id - 1) * ndata_per_mat + 1;
+    data_end_index = mat_id * ndata_per_mat;
+    data_end_index = min(data_end_index, ndata);
+    fprintf(['Start to generate training data indexed from ' num2str(data_start_index) ...
+    ' to ' num2str(data_end_index) '\n'])
+    data_name = [train_data_dir '/train_data_' num2str(data_start_index) '-' num2str(data_end_index) '.mat'];
+    if ~exist(data_name, 'file')
+        rng(mat_id)
+        coefs = sample_fc(cfg, ndata_per_mat);
+        uscat = complex(zeros(ndata_per_mat, n_dir, n_tgt));
+        for local_idx = 1:ndata_per_mat
+            coef = coefs(local_idx, :)';
+            src_info = geometries.starn(coef,nc,n);
             [mats,~] = rla.get_fw_mats(kh,src_info,bc,sensor_info,opts);
             fields = rla.compute_fields(kh,src_info,mats,sensor_info,bc,opts);
-            uscat = reshape(fields.uscat_tgt, [n_dir, n_tgt]);
-            save_fcn(data_name, coefs, uscat);
+            uscat_temp = reshape(fields.uscat_tgt, [n_dir, n_tgt]);
+            uscat(local_idx,:,:) = uscat_temp;
         end
+        save_fcn(data_name, coefs, uscat);
     end
 end
 time=toc
